a027244cab2827794286b980c88f147a71eb1ba5
Jakub <jakubszubzda150@gmail.com>
2025-10-06 16:49:27 +0200

    c

--- 5 Prefix Sums/CountDiv.cpp
+++ 5 Prefix Sums/CountDiv.cpp
@@ -1,7 +1,5 @@
-int solution(int A, int B, int K) {
-    int A_divisable = !(bool)(A % K),
-        B_divisable = !(bool)(B % K);
+#include <algorithm>
 
-    return ((B-B_divisable) - (A+A_divisable)) / K + A_divisable + (B_divisable && A != B);
-    // return ((B-!(bool)(B % K)) - (A+!(bool)(A % K))) / K + !(bool)(A % K) + (!(bool)(B % K) && A != B);
+int solution(int A, int B, int K) {
+    return B / K - std::max(A - 1, 0) / K + (A == 0);
 }
